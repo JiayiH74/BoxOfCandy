1f910ec08947d86e06c9fc1c412add9456246084
JiayiH74 <jiayih12@nycstudents.net>
2025-03-03 16:36:12 +0000

    Done

--- Main.java
+++ Main.java
@@ -1,7 +1,36 @@
 public class Main{
 
-public static void main(Strings[] args) {
+public static void main(String[] args) {
+    Candy[][] box = new Candy[4][3]; //arraylist
+    box[0][1] = new Candy("lime");
+    box[1][1] = new Candy("orange");
+    box[2][2] = new Candy("cherry");
+    box[3][1] = new Candy("lemon");
+    box[3][2] = new Candy("grape");
 
+    BoxOfCandy b = new BoxOfCandy(box);
+    System.out.println(b);
+    System.out.println(b.moveCandyToFirstRow(0));
+    System.out.println(b.moveCandyToFirstRow(1));
+    System.out.println(b.moveCandyToFirstRow(2));
+
+
+    box = new Candy[3][5];
+    box[0][0] = new Candy("lime");
+    box[0][1] = new Candy("lime");
+    box[0][3] = new Candy("lemon");
+    box[1][0] = new Candy("orange");
+    box[1][3] = new Candy("lime");
+    box[1][4] = new Candy("lime");
+    box[2][0] = new Candy("cherry");
+    box[2][2] = new Candy("lemon");
+    box[2][4] = new Candy("orange");
+
+    b = new BoxOfCandy(box);
+    System.out.println(b);
+    System.out.println(b.removeNextByFlavor("cherry"));
+    System.out.println(b);
+    System.out.println(b.removeNextByFlavor("lime"));
+    System.out.println(b.removeNextByFlavor("grape"));
   }
-
 }
